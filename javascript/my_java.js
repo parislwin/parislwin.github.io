4bef122ba4c4422484067a30d26258220189a989
parislwin <paristlwin@gmail.com>
2025-05-06 20:30:17 -0400

    update 4

---
+++ javascript/my_java.js
@@ -0,0 +1,8 @@
+<script>
+  const toggleButton = document.querySelector('.nav-toggle');
+  const navMenu = document.querySelector('.nav-menu');
+
+  toggleButton.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+  });
+</script>
